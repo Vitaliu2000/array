d5b1f4c616fb7954ecd1dcc2b456a5ce8a8e628c
Vitaliy Kosyuk <vitaliu2000@live.com>
2019-10-26 21:51:09 +0300

    commit: сыроватая версия домашки. Следующим комитом постараюсь исправиться.

---
+++ src/main/java/HomeWork/Simple.java
@@ -0,0 +1,42 @@
+package HomeWork;
+
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class Simple<E> implements Iterable<E> {
+    boolean add(E e) {
+        return false;
+    }
+
+    void delete(int index) {
+
+    }
+
+    E get(int index) {
+        return null;
+    }
+
+    int size() {
+        return 0;
+    }
+
+    void update(int index, E e) {
+
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public void forEach(Consumer<? super E> action) {
+
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        return null;
+    }
+}
